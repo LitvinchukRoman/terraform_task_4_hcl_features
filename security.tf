2d4197de0a34e6569d4f4f9d4da6b4f8198e55de
litvi <litvin4uk.roman@gmail.com>
2025-09-25 17:35:13 +0300

    Solution

--- security.tf
+++ security.tf
@@ -3,6 +3,7 @@ resource "azurerm_network_security_group" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
+
   dynamic "security_rule" {
     for_each = var.security_rules
     content {
